8df6d1bf3aa9a680ae66f80ee0f3e95786b401be
rghirri <57195308+rghirri@users.noreply.github.com>
2022-06-20 21:52:03 +0100

    collectionController completed need to flatend function

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -14,188 +14,49 @@ class CollectionController extends Controller
 {
     public function index()
     {
-        $json = Http::get('https://dev.shepherd.appoly.io/fruit.json')->json();
+        //$json = Http::get('https://dev.shepherd.appoly.io/fruit.json')->json();
 
-        $this->items = collect($json['menu_items']);
+        $response = Http::get('https://dev.shepherd.appoly.io/fruit.json');
+        
+        $data_json = $response->object();
+
+        $this->items = collect($data_json->menu_items);
         
         $collections =  $this->items;
 
-        
-        $collections->each(function ($value, $key) {
-          //var_dump($value['children']);
-           echo $value['label'] . ' -- ';
-            $value->each(function ($value, $key) {
-                echo $value['label'] . ' -- ';
-             });
-            //  $itemsOne = $value['children'];
-            //  var_dump($itemsOne);
-            //  $itemsOne->each(function ($value, $key) {
-            //      echo $value['label'] . ' -- ';
-            //   });
-        //     $itemsOne->each(function ($value, $key) {
-        //       echo $value['label'] . ' -- ';
-        //     if(!empty($itemsOne)){
-        //       echo "empty";
-               
-        //       echo $value['label'] . ' -- ';
-        //   });
-        //  }
-            
-        });
+        //$data = $collections->all();
 
-        //$collections->each(function ($value, $key) {
-          //var_dump($value['children']);
-          //  echo $value['label'] . ' -- ';
-          //   $value->each(function ($value, $key) {
-          //       echo $value['label'] . ' -- ';
-          //    });
-            //  $itemsOne = $value['children'];
-            //  var_dump($itemsOne);
-            //  $itemsOne->each(function ($value, $key) {
-            //      echo $value['label'] . ' -- ';
-            //   });
-        //     $itemsOne->each(function ($value, $key) {
-        //       echo $value['label'] . ' -- ';
-        //     if(!empty($itemsOne)){
-        //       echo "empty";
-               
-        //       echo $value['label'] . ' -- ';
-        //   });
-        //  }
-            
-       //});
-        
-        $collections->each(function ($value, $key) {
-          //var_dump($value['children']);
-           echo $value['label'] . ' -- ';
-            $value->each(function ($value, $key) {
-                echo $value['label'] . ' -- ';
-             });
-            //  $itemsOne = $value['children'];
-            //  var_dump($itemsOne);
-            //  $itemsOne->each(function ($value, $key) {
-            //      echo $value['label'] . ' -- ';
-            //   });
-        //     $itemsOne->each(function ($value, $key) {
-        //       echo $value['label'] . ' -- ';
-        //     if(!empty($itemsOne)){
-        //       echo "empty";
-               
-        //       echo $value['label'] . ' -- ';
-        //   });
-        //  }
-            
-        });
+        var_dump($collections);
 
-
-        // var_dump($collections);
-
-        //  return $collections->map(function ($itemFirst){
-        //     return $itemFirst->pluck('label')->sort()->all();
-        //     return $itemFirst->map(function ($itemSecond){
-        //       return $itemSecond->pluck('label')->sort()->all();
-        //     });
+        // return $collections->map(function ($item, $key) {
+        //   if (is_array($item)){
+        //     echo '--' . $item['label'];
+        //     echo '<br>';
+        //     continue;
+        //     }else
+        //     {
+        //     echo '--' . $item['label'];
+        //     echo '<br>';
+        //     }
+          // echo $item->label . '--';
+          // $itemObjects = $item->children;
+          // if (!empty($itemObject)){
+          // return   $itemObjects->map(function ($itemObject, $key) {
+          //     echo $itemObject->label . '--';
+          //   });
+          // }
         // });
 
-        // use map method to loop through collection of items
-        // Each array is sorted within a loop
-
-        //return $collections->pluck('label')->sort()->all();
-
-        // return $collection->each(function ($item, $key) {
-
-        //     return $item->get()->pluck('label')->sort();
-
-        // });
         
-      //  return $collections->map(function ($item){
-      //     return  collect($item)->only(['label', 'children'])->sort()->all();
-      //     // return $item;
+      //  return $collections->map(function ($item, $key) {
+      //     echo $item->label . '--';
+      //     $itemObjects = $item->children;
+      //     if (!empty($itemObject)){
+      //     return   $itemObjects->map(function ($itemObject, $key) {
+      //         echo $itemObject->label . '--';
+      //       });
+      //     }
       //   });
 
-
-        //  return $collection->mapWithKeys(function ($item){
-        //     return [
-        //         $item['label'];
-        //     ];
-        // });
-
-        // return $collection->map(function ($item){
-        //     return collect($item)->map(function ($child){
-        //         return collect($child)->only()
-        //     });
-        // });
-
-        //return $collection->pluck('label');
-
-        // $plucked = $collection->pluck('label');
-
-        // return $plucked->all();
-
-        // if ($collection->pluck('children')->isNotEmpty()){
-        //     return $collection->pluck('label');
-        //   }
-
-        //return $collection->pluck('children');
-
-        //  $users = User::all();
-
-        //  return $users
-        //          ->pluck('name')
-        //          ->except('id', 10)
-        //          ->skip(1);
-
-        // $collections->each(function ($value, $key) {
-        //     $item = $value['label'];
-        //     echo $item . ' | ';
-        // });
-
-        // $collection->map(function ($item, $key) {
-        //     print_r($item);
-        // });
-
-        // $filter = $collection->filter(function ($value, $key){
-        //     return $key == 'label';
-        // });
-
-        // print_r($filter->all());
-
-       // return $users;
-
-         //$json = Http::get('https://dev.shepherd.appoly.io/fruit.json')->json();
-
-        //var_dump($json);
-
-          //$this->items = collect($json['menu_items'])->all();
-        //  $collection =  $this->items;
-        
-        //$collection = collect(['name', 'language']);
-
-        //$combine = $collection->combine(['Dary', 'Laravel']);
-
-        //dd($collection->contains(6));
-
-        // $collection = collect([
-        //     ['name' => 'Dary'],
-        //     ['name' => 'John'],
-        //     ['name' => 'Michael']
-        // ]);
-
-        // if($collection->contains('name', 'Dary2')){
-        //     return 'True';
-        // }else
-        // {
-        //     return 'False';
-        // }
-
-        // $collection = collect([
-        //     'name'      => 'Dary',
-        //     'Job'       => 'Web Developer',
-        //     'country'   => 'The Netherlands'
-        // ]);
-
-        // return $collection->forget('Job');
-
-    }
-    
+  }
 }
